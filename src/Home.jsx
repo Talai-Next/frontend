a7725d90746f63969f4be0242c374f88f504d9b9
Napasorn TEVARUT <napasorn.te@ku.th>
2025-02-12 23:38:18 +0700

    add Header component

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,36 +6,14 @@ import { TbReportAnalytics } from "react-icons/tb";
 import { IoColorFillOutline } from "react-icons/io5";
 import './index.css'
 import TextField from "@mui/material/TextField";
-
+import Header from './components/Header';
 function Home() {
 
   return (
       <div className='flex flex-col w-screen'>
-        <div className='mt-5 mb-2 flex justify-center items-center'>
-            <h2 className='font-bold'>Talai NEXT</h2>
-        </div>
-        <div className='my-2 mx-[5px] flex justify-between'>
-            <div className='flex flex-col w-[30%] justify-center items-center'>
-                <LiaHomeSolid size={36}/>
-                <h4>Home</h4>
-            </div>
-            <div className='flex flex-col w-[30%] justify-center items-center'>
-                <TbReportAnalytics size={36}/>
-                <h4>Report</h4>
-            </div>
-            <div className=' flex flex-col w-[30%] justify-center items-center'>
-                <IoColorFillOutline size={36}/>
-                <h4>Theme</h4>
-            </div>
+        <Header />
+        <div>
             
-        </div>
-        <div className='my-2 mx-5'>
-            <TextField
-                id="outlined-basic"
-                variant="outlined"
-                fullWidth
-                label="Search"
-                />
         </div>
       </div>
   )
